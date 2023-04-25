d610a54688887a523fc2e6683a3d165db6b67e4b
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-04-24 21:56:14 -0300

    blog page

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -1,19 +1,26 @@
 import GetPostModal from '../modals/post/getPost';
+import AddPostModal from '../modals/post/addPost';
 import styles from './styles.module.scss';
+import * as Icons from 'react-ionicons';
 import Navbar from "../navbar";
 import React from "react";
 
 export default function Blog() {
 
     const [getPostModal, setGetPostModal] = React.useState(false);
+    const [addPostModal, setAddPostModal] = React.useState(false);
 
     return (
         <>
             <GetPostModal open={getPostModal} handleClose={() => setGetPostModal(false)} />
+            <AddPostModal open={addPostModal} handleClose={() => setAddPostModal(false)} />
             <Navbar />
             <div className={styles.container}>
                 <div className={styles.content}>
-                    <h1 className={styles.title}>Blog</h1>
+                    <div className={styles.header}>
+                        <h1 className={styles.title}>Blog</h1>
+                        <button className={styles.addButton} onClick={() => setAddPostModal(true)}><Icons.AddOutline width="35px" height="35px" color="#fff" /></button>
+                    </div>
                     <div className={styles.posts}>
                         <div className={styles.post} onClick={() => setGetPostModal(true)}>
                             <p className={styles.postDescription}>A educação sexual é um processo de aprendizagem que visa a formação de uma consciência crítica sobre a sexualidade, ajudando a pessoa a tomar decisões responsáveis e saudáveis sobre o seu comportamento sexual.</p>
