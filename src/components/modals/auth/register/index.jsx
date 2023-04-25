7afb1e5a4489a8ec6847faffa33a46d6ed1efee0
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-04-24 22:02:49 -0300

    Register

---
+++ src/components/modals/auth/register/index.jsx
@@ -0,0 +1,64 @@
+import styles from './styles.module.scss';
+import Modal from '@mui/material/Modal';
+import * as Icons from 'react-ionicons';
+import React from 'react';
+
+export default function RegisterModal({ open, handleClose }) {
+
+    const [data, setData] = React.useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            hideBackdrop
+            disableEscapeKeyDown
+            className={styles.modal}
+        >
+            <div className={styles.container}>
+                <button className={styles.close} onClick={handleClose}>
+                    <Icons.CloseOutline width="30px" height="30px" color="#8C89B8" />
+                </button>
+                <h1 className={styles.title}>Cadastrar</h1>
+                <form className={styles.form}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        name="name"
+                        placeholder="Nome"
+                        value={data.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        className={styles.input}
+                        type="email"
+                        name="email"
+                        placeholder="E-mail"
+                        value={data.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        className={styles.input}
+                        type="password"
+                        name="password"
+                        placeholder="Senha"
+                        value={data.password}
+                        onChange={handleChange}
+                    />
+                    <button className={styles.button} type="submit">Cadastrar</button>
+                </form>
+            </div>
+        </Modal>
+    );
+}
