22564968b0ea8b489bb524dee0efc54e0aa3b4e2
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-04-24 21:56:21 -0300

    add post

---
+++ src/components/modals/post/addPost/index.jsx
@@ -0,0 +1,54 @@
+import styles from './styles.module.scss';
+import Modal from '@mui/material/Modal';
+import * as Icons from 'react-ionicons';
+import React from 'react';
+
+export default function AddPostModal({ open, handleClose }) {
+
+    const [data, setData] = React.useState({
+        title: '',
+        description: '',
+    });
+
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    return (
+        <Modal
+            open={open}
+            onClose={handleClose}
+            hideBackdrop
+            disableEscapeKeyDown
+            className={styles.modal}
+        >
+            <div className={styles.container}>
+                <button className={styles.close} onClick={handleClose}>
+                    <Icons.CloseOutline width="30px" height="30px" color="#8C89B8" />
+                </button>
+                <h1 className={styles.title}>Adicionar post</h1>
+                <form className={styles.form}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        name="title"
+                        placeholder="Título"
+                        value={data.title}
+                        onChange={handleChange}
+                    />
+                    <textarea
+                        className={styles.textarea}
+                        name="description"
+                        placeholder="Descrição"
+                        value={data.description}
+                        onChange={handleChange}
+                    />
+                    <button className={styles.button} type="submit">Adicionar</button>
+                </form>
+            </div>
+        </Modal>
+    );
+}
