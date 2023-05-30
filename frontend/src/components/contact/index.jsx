e66eb3a058989e08200fba9bb1e793de84ddb84e
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-05-29 21:23:58 -0300

    Revert "Revert "front""
    This reverts commit 1d57cf586eb90323de4734715d20ffc7e74a0d88.

---
+++ frontend/src/components/contact/index.jsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+import * as Icons from 'react-ionicons';
+import Navbar from "../navbar";
+import React from "react";
+
+export default function Contact() {
+
+    return (
+        <>
+            <Navbar />
+            <div className={styles.container}>
+            </div>
+        </>
+    );
+}
