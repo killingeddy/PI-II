0876dd7c516dc67683f3b5b9e2a9493664414232
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-05 19:20:30 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,10 @@ const cors = require("cors");
 require('dotenv').config();
 const app = express();
 
+app.use(cors({ credentials: true, origin: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
-app.use(cors({ credentials: true, origin: true }));
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
 
 app.listen(process.env.PORT, () => {
     console.log('_____Start_____')
