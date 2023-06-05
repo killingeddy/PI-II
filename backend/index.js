3f4c2fa03562fa70fa336a9e128c3f878500bf02
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-05 10:28:12 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
