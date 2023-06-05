e9f17cd01643de7dcc6924cdf533d0621450a070
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-05 10:13:03 -0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const router = require("./routes/router");
 const express = require("express");
+const cors = require("cors");
 require('dotenv').config();
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.listen(process.env.PORT, ()=>{
     console.log('_____Start_____')
