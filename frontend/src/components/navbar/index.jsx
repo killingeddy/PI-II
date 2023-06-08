24b432003d94daeeaec3217d082e5eeee5f40eb0
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-08 12:27:52 -0300

    nova sidebar, logos e icones

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,43 +1,84 @@
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import LoginModal from "../modals/auth/login";
 import styles from "./styles.module.scss";
 import * as Icons from 'react-ionicons';
+import { useRouter } from 'next/router';
 import React from "react";
+import Link from 'next/link';
 
 export default function Navbar() {
 
-    const [sidebar, setSidebar] = React.useState(false);
+    const [sidebar, setSidebar] = React.useState(true);
     const [login, setLogin] = React.useState(false);
 
+    const [logged, setLogged] = React.useState(false);
+
+    const router = useRouter();
+
+    const localVerify = () => {
+        if (localStorage.getItem("token")) {
+            setLogged(true);
+        }
+    }
+
+    React.useEffect(() => {
+        localVerify();
+    }, []);
+
     return (
-        <>
+        <div className={styles.sidebar} onMouseEnter={() => setSidebar(false)} onMouseLeave={() => setSidebar(true)}>
             <LoginModal open={login} handleClose={() => setLogin(false)} />
-            <div className={styles.navbar}>
-                <h1 onClick={() => window.location.href = '/'} className={styles.title}>A aula proibida</h1>
-                <button className={styles.button} onClick={() => setSidebar(true)}><Icons.MenuOutline width="30px" height="30px" color="#8C89B8" /></button>
-            </div>
-            {
-                sidebar &&
-                <div className={styles.sidebar}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8C89B870" fillOpacity="1" d="M0,96L48,128C96,160,192,224,288,250.7C384,277,480,267,576,245.3C672,224,768,192,864,170.7C960,149,1056,139,1152,165.3C1248,192,1344,256,1392,288L1440,320L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-                    <button className={styles.button} onClick={() => setSidebar(false)}><Icons.CloseOutline width="30px" height="30px" color="#000" /></button>
-                    <div className={styles.itens}>
-                        {
-                            !localStorage.getItem('token')
+            <Sidebar collapsed={sidebar} style={{ backgroundColor: "#f5f5f5", height: "100vh" }} transitionDuration={800}>
+                <Menu>
+                    <img src="/logo.png" alt="logo" className={styles.logo} />
+                </Menu>
+                <Menu
+                    menuItemStyles={{
+                        icon: { marginTop: "10px", marginRight: "10px" },
+                        label: { color: "#8C89B8" },
+                        button: ({ active }) => ({
+                            background: active
                                 ?
-                                <button onClick={() => setLogin(true)} className={styles.item}><Icons.LogInOutline color='#8C89B8' width="25px" height="25px" /><p>Login</p></button>
+                                'linear-gradient(340deg, rgba(140, 137, 184, 0.5019257703081232) 0%, rgba(239, 195, 230, 0.499124649859944) 100%);'
                                 :
-                                <button onClick={() => setLogin(true)} className={styles.item}><Icons.PersonOutline color='#8C89B8' width="25px" height="25px" /><p>Meu Perfil</p></button>
-                        }
-                        <button onClick={() => window.location.href = '/'} className={styles.item}><Icons.HomeOutline color='#8C89B8' width="25px" height="25px" /><p>Home</p></button>
-                        <a href="#about" className={styles.item}><Icons.InformationCircleOutline color='#8C89B8' width="25px" height="25px" /><p>Sobre</p></a>
-                        <a href="#contact" className={styles.item}><Icons.CallOutline color='#8C89B8' width="25px" height="25px" /><p>Contato</p></a>
-                        {
-                            localStorage.getItem('token') &&
-                            <button onClick={() => { localStorage.clear(), window.location.href = '/' }} className={styles.item}><Icons.LogOutOutline color='#8C89B8' width="25px" height="25px" /><p>Sair</p></button>
-                        }
-                    </div>
-                </div>
-            }
-        </>
+                                "#f5f5f5",
+                            border: "none",
+                            borderTopLeftRadius: "40px",
+                            borderBottomLeftRadius: "40px",
+                            padding: "10px 20px",
+                            margin: "10px",
+                            width: "100%",
+                            ":hover": { background: "#c3c3c3" }
+                        })
+                    }}
+                >
+                    <Link href="/">
+                        <MenuItem active={router.pathname === "/"} icon={<Icons.HomeOutline color={'#8C89B8'} />}> <p className={styles.text}>Inicio</p></MenuItem>
+                    </Link>
+                    {
+                        !logged
+                            ?
+                            <MenuItem icon={<Icons.LogInOutline color={'#8C89B8'} />} onClick={() => setLogin(true)}> <p className={styles.text}>Login</p></MenuItem>
+                            :
+                            <MenuItem icon={<Icons.PersonOutline color={'#8C89B8'} />} onClick={() => setLogin(true)}> <p className={styles.text}>Meu perfil</p></MenuItem>
+                    }
+                    <Link href="/blog">
+                        <MenuItem active={router.pathname === "/blog"} icon={<Icons.LibraryOutline color={'#8C89B8'} />}> <p className={styles.text}>Blog</p></MenuItem>
+                    </Link>
+                    <Link href="/contact">
+                        <MenuItem active={router.pathname === "/contact"} icon={<Icons.CallOutline color={'#8C89B8'} />}> <p className={styles.text}>Contato</p></MenuItem>
+                    </Link>
+                    {
+                        logged
+                        &&
+                        <MenuItem icon={<Icons.LogOutOutline color={'#8C89B8'} />} onClick={() => {
+                            localStorage.clear();
+                            setLogged(false);
+                            router.push("/");
+                        }}> <p className={styles.text}>Sair</p></MenuItem>
+                    }
+                </Menu>
+            </Sidebar>
+        </div>
     );
 }
