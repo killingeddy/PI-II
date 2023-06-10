36a4f6b88b2b4be76f38c2888e4e3504c8c4ea67
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-10 13:16:58 -0300

    contact page

--- frontend/src/components/contact/index.jsx
+++ frontend/src/components/contact/index.jsx
@@ -9,6 +9,22 @@ export default function Contact() {
         <>
             <Navbar />
             <div className={styles.container}>
+                <div className={styles.main}>
+                    <div className={styles.content}>
+                        <div className={styles.leftItem}>
+                            <h2>Obrigado por acessar nosso site!</h2>
+                            <p>Entre em contato conosco para mais informações sobre nossa platarfoma, tirar suas dúvidas e nos ajudar a melhorar cada vez mais.</p>
+                            <img className={styles.image} src="../../../logo.png" alt="Logo" />
+                        </div>
+                        <div className={styles.rightItem}>
+                            <input className={styles.input} type="text" placeholder="Nome" />
+                            <input className={styles.input} type="text" placeholder="E-mail" />
+                            <input className={styles.input} type="text" placeholder="Assunto" />
+                            <textarea className={styles.input} style={{ height: '200px' }} type="text" placeholder="Mensagem" />
+                            <button className={styles.button}>Enviar</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
