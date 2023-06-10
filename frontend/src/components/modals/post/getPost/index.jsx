27527de19086822a41e38cf7160699ffd8c72158
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-10 13:25:02 -0300

    Revert "get post component"
    This reverts commit e4d89a2a7cd9725789404564d9dddee2cdec85ce.

--- frontend/src/components/modals/post/getPost/index.jsx
+++ frontend/src/components/modals/post/getPost/index.jsx
@@ -2,57 +2,10 @@ import styles from './styles.module.scss';
 import Modal from '@mui/material/Modal';
 import * as Icons from 'react-ionicons';
 import React from 'react';
-import api from '@/tools/api';
-import { ToastContainer, toast } from 'react-toastify';
 
-export default function GetPostModal({ open, handleClose, id }) {
+export default function GetPostModal({ open, handleClose }) {
 
-    const [data, setData] = React.useState({});
-    const [comments, setComments] = React.useState([]);
-
-    const getPost = async () => {
-        api
-            .get(`/posts/${id}`)
-            .then((response) => {
-                setData(response.data[0]);
-            })
-            .catch((error) => {
-                toast.error('Erro ao acessar post', {
-                    position: "top-left",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
-            });
-    };
-
-    const getComments = async () => {
-        api
-            .get(`/posts/${id}/comments`)
-            .then((response) => {
-                setComments(response.data);
-            })
-            .catch((error) => {
-                toast.error('Erro ao acessar post', {
-                    position: "top-left",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "dark",
-                });
-            });
-    };
-
-    React.useEffect(() => {
-        getPost();
-    }, [open]);
+    const [liked, setLiked] = React.useState(false);
 
     return (
         <Modal
@@ -66,32 +19,45 @@ export default function GetPostModal({ open, handleClose, id }) {
                 <button className={styles.close} onClick={handleClose}>
                     <Icons.CloseOutline width="30px" height="30px" color="#8C89B8" />
                 </button>
-                <h1 className={styles.title}>{data?.title}</h1>
-                <h2 className={styles.subtitle}>Autor: {data?.name}</h2>
-                <p className={styles.description}>{data?.post_content}</p>
+                <h1 className={styles.title}>Lorem Ipsum</h1>
+                <h2 className={styles.subtitle}>Autor: Lorens Ispsuns</h2>
+                <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
                 <div className={styles.buttons}>
                     <button className={styles.button} onClick={() => setLiked(!liked)}>
-                        {/* {
+                        {
                             liked ? <Icons.Heart width="20px" height="20px" color="#8C89B8" /> : <Icons.HeartOutline width="20px" height="20px" color="#8C89B8" />
-                        } */}
+                        }
                     </button>
                     <button className={styles.button}><Icons.ChatbubbleOutline width="20px" height="20px" color="#8C89B8" /></button>
                     <button className={styles.button}><Icons.BookmarkOutline width="20px" height="20px" color="#8C89B8" /></button>
                     <button className={styles.button}><Icons.ShareSocialOutline width="20px" height="20px" color="#8C89B8" /></button>
                 </div>
                 <div className={styles.comments}>
-                    {
-                        comments?.map((item, index) => {
-                            return (
-                                <div className={styles.comment} key={index}>
-                                    <h3 className={styles.commentTitle}>{item.name}</h3>
-                                    <p className={styles.commentDescription}>{item.comment_content}</p>
-                                </div>
-                            )
-                        })
-                    }
+                    <div className={styles.comment}>
+                        <h3 className={styles.commentTitle}>Lorem Ipsum</h3>
+                        <p className={styles.commentDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
+                    </div>
+                    <div className={styles.comment}>
+                        <h3 className={styles.commentTitle}>Lorem Ipsum</h3>
+                        <p className={styles.commentDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
+                    </div>
+                    <div className={styles.comment}>
+                        <h3 className={styles.commentTitle}>Lorem Ipsum</h3>
+                        <p className={styles.commentDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
+                    </div>
+                    <div className={styles.comment}>
+                        <h3 className={styles.commentTitle}>Lorem Ipsum</h3>
+                        <p className={styles.commentDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
+                    </div>
+                    <div className={styles.comment}>
+                        <h3 className={styles.commentTitle}>Lorem Ipsum</h3>
+                        <p className={styles.commentDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
+                    </div>
+                    <div className={styles.comment}>
+                        <h3 className={styles.commentTitle}>Lorem Ipsum</h3>
+                        <p className={styles.commentDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl. Sed euismod, diam quis aliquam tincidunt, nunc elit ultricies nunc, nec aliquam nisl nunc eget nisl.</p>
+                    </div>
                 </div>
-                <ToastContainer />
             </div>
         </Modal>
     );
