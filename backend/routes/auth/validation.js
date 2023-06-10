00e2955f09c6176c2b771cdcb53fbcafb1c19fd6
Eduardo Medrado <100213506+eduiseddie@users.noreply.github.com>
2023-06-10 13:51:28 -0300

    validation schema

--- backend/routes/auth/validation.js
+++ backend/routes/auth/validation.js
@@ -1,19 +1,19 @@
 const joi = require('joi');
 
 const registerSchema = joi.object({
-    email: joi.string().min(8).max(200).required().email(),
-    name: joi.string().min(3).max(150).required().pattern(new RegExp('^[a-zA-Z ]+$')),
+    email: joi.string().max(200).required().email(),
+    name: joi.string().max(150).required().pattern(new RegExp('^[a-zA-Z ]+$')),
     password: joi.string().min(6).max(50).required()
 });
 
 const loginSchema = joi.object({
-    email: joi.string().min(8).max(200).required().email(),
+    email: joi.string().max(200).required().email(),
     password: joi.string().min(6).max(50).required()
 });
 
 const editSchema = joi.object({
-    email: joi.string().min(8).max(200).required().email(),
-    name: joi.string().min(3).max(150).required().pattern(new RegExp('^[a-zA-Z ]+$')),
+    email: joi.string().max(200).required().email(),
+    name: joi.string().max(150).required().pattern(new RegExp('^[a-zA-Z ]+$')),
     password: joi.string().min(6).max(50).required()
 });
 
--- backend/routes/posts/validation.js
+++ backend/routes/posts/validation.js
@@ -1,18 +1,18 @@
 const joi = require('joi');
 
 const addPostSchema = joi.object({
-    title: joi.string().min(3).max(50).allow(null, ''),
-    post_content: joi.string().min(3).max(255).required(),
+    title: joi.string().max(50).allow(null, ''),
+    post_content: joi.string().max(255).required(),
     user_id: joi.number().integer().required()
 });
 
 const editPostSchema = joi.object({
-    title: joi.string().min(3).max(50).allow(null, ''),
-    post_content: joi.string().min(3).max(255).required()
+    title: joi.string().max(50).allow(null, ''),
+    post_content: joi.string().max(255).required()
 });
 
 const addCommentSchema = joi.object({
-    comment_content: joi.string().min(3).max(255).required(),
+    comment_content: joi.string().max(255).required(),
     user_id: joi.number().integer().required()
 });
 
